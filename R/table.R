5271cb69399c79de5b3d69718c40603f0bff7a3e
Olav Nikolai Risdal Breivik <olavbr@nr.no>
2024-03-21 13:47:31 +0100

    much cleaning in model tables

--- R/table.R
+++ R/table.R
@@ -4,80 +4,49 @@
 #' @param CI_level Confidence level to accompany the point estimates with
 #' @return Table with model parameters
 #' @export
-partable_model<-function(run,CI_level=0.95){
+partable<-function(run,CI_level=0.95){
 
   CI_L <- (1-CI_level)/2
   CI_U <- 1-CI_L
+  qq = c(qnorm(CI_L),qnorm(CI_U))
+  pl = run$pl
+  plSd = run$plSd
 
-  map = run$map
-  pl = as.list(run$rep,"Est")
-  plSd = as.list(run$rep,"Std")
-
-  sigma_int = c(exp(pl$log_sigma[1]),
-                exp(pl$log_sigma[1] + qnorm(CI_L)*plSd$log_sigma[1]),
-                exp(pl$log_sigma[1] + qnorm(CI_U)*plSd$log_sigma[1]))
-  log_kappa_int = c(pl$log_kappa[1],
-                pl$log_kappa[1] + qnorm(CI_L)*plSd$log_kappa[1],
-                pl$log_kappa[1] + qnorm(CI_U)*plSd$log_kappa[1])
-  sigma_size = c(exp(pl$log_sigma[2]),
-                 exp(pl$log_sigma[2] + qnorm(CI_L)*plSd$log_sigma[2]),
-                 exp(pl$log_sigma[2] + qnorm(CI_U)*plSd$log_sigma[2]))
-  log_kappa_size = c(pl$log_kappa[2],
-                 pl$log_kappa[2] + qnorm(CI_L)*plSd$log_kappa[2],
-                 pl$log_kappa[2] + qnorm(CI_U)*plSd$log_kappa[2])
-  mu1 = c(exp(pl$log_mu[1]),
-          exp(pl$log_mu[1] + qnorm(CI_L)*plSd$log_mu[1]),
-          exp(pl$log_mu[1] + qnorm(CI_U)*plSd$log_mu[1]))
-  mu2 = c(exp(pl$log_mu[2]),
-          exp(pl$log_mu[2] + qnorm(CI_L)*plSd$log_mu[2]),
-          exp(pl$log_mu[2] + qnorm(CI_U)*plSd$log_mu[2]))
-  c_mmpp = c(exp(pl$log_c_mmpp)+1,
-             exp(pl$log_c_mmpp + qnorm(CI_L)*plSd$log_c_mmpp)+1,
-             exp(pl$log_c_mmpp + qnorm(CI_U)*plSd$log_c_mmpp)+1)
-  betaz = c(pl$beta_z[1],
-            pl$beta_z[1] + qnorm(CI_L)*plSd$beta_z[1],
-            pl$beta_z[1] + qnorm(CI_U)*plSd$beta_z[1])
-  betaG1 = c(pl$beta_g[1],
-             pl$beta_g[1] + qnorm(CI_L)*plSd$beta_g[1],
-             pl$beta_g[1] + qnorm(CI_U)*plSd$beta_g[1])
-  betaG2 = c(pl$beta_g[2],
-             pl$beta_g[2] + qnorm(CI_L)*plSd$beta_g[2],
-             pl$beta_g[2] + qnorm(CI_U)*plSd$beta_g[2])
-  betaG3 = c(pl$beta_g[3],
-             pl$beta_g[3] + qnorm(CI_L)*plSd$beta_g[3],
-             pl$beta_g[3] + qnorm(CI_U)*plSd$beta_g[3])
+  sigma_int = exp(c(pl$log_sigma[1], pl$log_sigma[1] + qq*plSd$log_sigma[1]))
+  log_kappa_int = c(pl$log_kappa[1], pl$log_kappa[1] + qq*plSd$log_kappa[1])
+  sigma_size = exp(c(pl$log_sigma[2], pl$log_sigma[2] + qq*plSd$log_sigma[2]))
+  log_kappa_size = c(pl$log_kappa[2], pl$log_kappa[2] + qq*plSd$log_kappa[2])
+  mu1 = exp(c(pl$log_mu[1], pl$log_mu[1] + qq*plSd$log_mu[1]))
+  mu2 = exp(c(pl$log_mu[2], pl$log_mu[2] + qq*plSd$log_mu[2]))
+  c_mmpp = c(exp(pl$log_c_mmpp)+1, exp(pl$log_c_mmpp +  qq*plSd$log_c_mmpp)+1)
+  betaz = c(pl$beta_z[1], pl$beta_z[1] +  qq*plSd$beta_z[1])
+  betaG1 = c(pl$beta_g[1], pl$beta_g[1] + qq*plSd$beta_g[1])
+  betaG2 = c(pl$beta_g[2], pl$beta_g[2] + qq*plSd$beta_g[2])
+  betaG3 = c(pl$beta_g[3], pl$beta_g[3] + qq*plSd$beta_g[3])
 
   if(run$conf$g_function==2){
-    b = exp(c(pl$logB, 
-              pl$logB + qnorm(CI_L)*plSd$logB,
-              pl$logB + qnorm(CI_U)*plSd$logB ))
+    b = exp(c(pl$logB, pl$logB + qq*plSd$logB ))
   }
     
-  beta0_size = c(pl$beta_size[1],
-                 pl$beta_size[1] + qnorm(CI_L)*plSd$beta_size[1],
-                 pl$beta_size[1] + qnorm(CI_U)*plSd$beta_size[1])
-  beta1_size = c(pl$beta_size[2],
-                 pl$beta_size[2] + qnorm(CI_L)*plSd$beta_size[2],
-                 pl$beta_size[2] + qnorm(CI_U)*plSd$beta_size[2])
-  log_size_NB = c(pl$logSizeNB,
-                pl$logSizeNB + qnorm(CI_L)*plSd$logSizeNB,
-                pl$logSizeNB + qnorm(CI_U)*plSd$logSizeNB)
+  beta0_size = c(pl$beta_size[1],pl$beta_size[1] + qq*plSd$beta_size[1])
+  beta1_size = c(pl$beta_size[2],pl$beta_size[2] + qq*plSd$beta_size[2])
+  log_size_NB = c(pl$logSizeNB,pl$logSizeNB + qq*plSd$logSizeNB)
 
   parTab = rbind(sigma_int,log_kappa_int,mu1,mu2,c_mmpp,betaz,betaG1,betaG2,betaG3,b,sigma_size,log_kappa_size,beta0_size,beta1_size,log_size_NB)
   parTab <- as.data.frame(parTab)
   colnames(parTab) <- c("Est","CI_L","CI_U")
 
   # Setting estimates to NA if not estimated
-  if(!is.null(map$x)){
+  if(!is.null(run$map$x)){
     parTab[rownames(parTab)=="sigma_int",] = NA
     parTab[rownames(parTab)=="log_kappa_int",] = NA
   }
-  if(!is.null(map$log_mu)){
+  if(!is.null(run$map$log_mu)){
     parTab[rownames(parTab)=="mu1",] = NA
     parTab[rownames(parTab)=="mu2",] = NA
     parTab[rownames(parTab)=="c_mmpp",] = NA
   }
-  if(is.na(map$log_sigma[1])){
+  if(is.na(run$map$log_sigma[1])){
     parTab[rownames(parTab)=="sigma_int",] = NA
     parTab[rownames(parTab)=="log_kappa_int",] = NA
   }
@@ -89,72 +58,28 @@ partable_model<-function(run,CI_level=0.95){
 #' Return table with derived model parameters
 #' 
 #' @param run run to extract model parameters from
-#' @param sdreport_run the sdreport object for the run to extract model parameters from
+#' @param sdrep_bc the biascorrected sdreport object for the run to extract model parameters from
 #' @param CI_level Confidence level to accompany the point estimates with
 #' @return Table with derived model parameters
 #' @export
-partable_model_derived <-function(run,sdreport_run = NULL,CI_level=0.95){
-
+partable_derived <-function(run,sdrep_bc = NULL,CI_level=0.95){
   CI_L <- (1-CI_level)/2
   CI_U <- 1-CI_L
-
-  map = run$map
-  pl = as.list(run$rep,"Est")
-  plSd = as.list(run$rep,"Std")
-  rl = as.list(run$rep,"Est", report = TRUE)
-  rlSd = as.list(run$rep,"Std", report = TRUE)
+  qq = c(qnorm(CI_L),qnorm(CI_U))
   
-  if(!is.null(sdreport_run)){
-    rlBiasCorr = as.list(sdreport_run,"Est. (bias.correct)", report = TRUE)
-    rlBiasCorrSd = as.list(sdreport_run,"Std. (bias.correct)", report = TRUE)
-  }
-
-
-
-  range_int = c(sqrt(8)/exp(pl$log_kappa[1]),
-            sqrt(8)/exp(pl$log_kappa[1] + qnorm(CI_U)*plSd$log_kappa[1]), # CI_U here as we divide by kappa
-            sqrt(8)/exp(pl$log_kappa[1] + qnorm(CI_L)*plSd$log_kappa[1])) # CI_L here as we divide by kappa
-
-  betaG1 = c(pl$beta_g[1],
-             pl$beta_g[1] + qnorm(CI_L)*plSd$beta_g[1],
-             pl$beta_g[1] + qnorm(CI_U)*plSd$beta_g[1])
-  betaG2 = c(pl$beta_g[2],
-             pl$beta_g[2] + qnorm(CI_L)*plSd$beta_g[2],
-             pl$beta_g[2] + qnorm(CI_U)*plSd$beta_g[2])
-  betaG3 = c(pl$beta_g[3],
-             pl$beta_g[3] + qnorm(CI_L)*plSd$beta_g[3],
-             pl$beta_g[3] + qnorm(CI_U)*plSd$beta_g[3])
-
-  half_stripe_widthG1 <- sqrt(2*pi*exp(betaG1))/2
-  half_stripe_widthG2 <- sqrt(2*pi*exp(betaG2))/2
-  half_stripe_widthG3 <- sqrt(2*pi*exp(betaG3))/2
-
-  range_psi = c(exp(rl$log_range_psi), 
-                exp(rl$log_range_psi+ qnorm(CI_L)*rlSd$log_range_psi),
-                exp(rl$log_range_psi+ qnorm(CI_U)*rlSd$log_range_psi))
-
-
-  k_psi = c(rl$k_psi,
-            rl$k_psi + qnorm(CI_L)*rlSd$k_psi,
-            rl$k_psi + qnorm(CI_U)*rlSd$k_psi)
-
-  range_size = c(sqrt(8)/exp(pl$log_kappa[2]),
-                 sqrt(8)/exp(pl$log_kappa[2] + qnorm(CI_U)*plSd$log_kappa[2]), # CI_U here as we divide by kappa
-                 sqrt(8)/exp(pl$log_kappa[2] + qnorm(CI_L)*plSd$log_kappa[2])) # CI_L here as we divide by kappa
-
-  meanGroupSize = c(rl$meanGroupSize,
-                    rl$meanGroupSize + qnorm(CI_L)*rlSd$meanGroupSize,
-                    rl$meanGroupSize + qnorm(CI_U)*rlSd$meanGroupSize)
-
-
-  abundance = c(exp(rl$logAbundance),
-                exp(rl$logAbundance + qnorm(CI_L)*rlSd$logAbundance),
-                exp(rl$logAbundance + qnorm(CI_U)*rlSd$logAbundance))
-
-  if(!is.null(sdreport_run)){
-    abundanceBiasCorrected = c(exp(rlBiasCorr$logAbundance),
-                  exp(rlBiasCorr$logAbundance + qnorm(CI_L)*rlBiasCorrSd$logAbundance),
-                  exp(rlBiasCorr$logAbundance + qnorm(CI_U)*rlBiasCorrSd$logAbundance))
+  range_int = c(sqrt(8)/exp(run$pl$log_kappa[1]),sqrt(8)/exp(run$pl$log_kappa[1] +  rev(qq)*run$plSd$log_kappa[1])) 
+  half_stripe_widthG1 = exp(c(run$rl$logEswReport[1], run$rl$logEswReport[1] + qq*run$rlSd$logEswReport[1]))
+  half_stripe_widthG2 = exp(c(run$rl$logEswReport[2], run$rl$logEswReport[2] + qq*run$rlSd$logEswReport[2]))
+  half_stripe_widthG3 = exp(c(run$rl$logEswReport[3], run$rl$logEswReport[3] + qq*run$rlSd$logEswReport[3]))
+  range_psi = exp(c(run$rl$log_range_psi[1], run$rl$log_range_psi[1]+ qq*run$rlSd$log_range_psi[1]))
+  k_psi = c(run$rl$k_psi, run$rl$k_psi[1] + qq*run$rlSd$k_psi[1])
+  range_size = c(sqrt(8)/exp(run$pl$log_kappa[2]),sqrt(8)/exp(run$pl$log_kappa[2] + rev(qq)*run$plSd$log_kappa[2]))
+  meanGroupSize = c(run$rl$meanGroupSize[1],run$rl$meanGroupSize[1] +qq*run$rlSd$meanGroupSize[1])
+  abundance = exp(c(run$rl$logAbundance[1],run$rl$logAbundance[1] + qq*run$rlSd$logAbundance[1]))
+  if(!is.null(sdrep_bc)){
+    rlBiasCorr = as.list(sdrep_bc,"Est. (bias.correct)", report = TRUE)
+    rlBiasCorrSd = as.list(sdrep_bc,"Std. (bias.correct)", report = TRUE)
+    abundanceBiasCorrected = exp(c(rlBiasCorr$logAbundance[1], rlBiasCorr$logAbundance[1] + qq*rlBiasCorrSd$logAbundance[1]))
   }else{
     abundanceBiasCorrected = c(NA,NA,NA)
   }
@@ -162,19 +87,14 @@ partable_model_derived <-function(run,sdreport_run = NULL,CI_level=0.95){
   parTab = rbind(range_int,half_stripe_widthG1,half_stripe_widthG2,half_stripe_widthG3,range_psi,k_psi,range_size,meanGroupSize,abundance,abundanceBiasCorrected)
   parTab <- as.data.frame(parTab)
   colnames(parTab) <- c("Est","CI_L","CI_U")
-
   # Setting estimates to NA if not estimated
-  if(!is.null(map$x)){
+  if(!is.null(run$map$x)){
     parTab[rownames(parTab)=="range_int",] = NA
   }
-
-  if(!is.null(map$log_mu)){
+  if(!is.null(run$map$log_mu)){
     parTab[rownames(parTab)=="range_psi",] = NA
     parTab[rownames(parTab)=="k_psi",] = NA
   }
-
-  
-
   return(parTab)
 }
 
